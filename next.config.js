f76ed74a9c483efb6e80f260fde9a420d4671c31
ce-meaux <94109971+christ-embassy-church@users.noreply.github.com>
2022-01-26 10:57:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 };
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : ''
+}
